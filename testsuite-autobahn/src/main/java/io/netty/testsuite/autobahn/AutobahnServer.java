9e0b4f5df28c5a3d18d8efd863651f7945e3a36c
CNan <fwq1991@yealink.com>
2020-03-23 16:19:39 +0800

    重命名

--- testsuite-autobahn/src/main/java/io/netty/testsuite/autobahn/AutobahnServer.java
+++ testsuite-autobahn/src/main/java/io/netty/testsuite/autobahn/AutobahnServer.java
@@ -54,13 +54,13 @@ public class AutobahnServer {
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        int port;
-        if (args.length > 0) {
-            port = Integer.parseInt(args[0]);
-        } else {
-            port = 9000;
-        }
-        new AutobahnServer(port).run();
-    }
+//    public static void main(String[] args) throws Exception {
+//        int port;
+//        if (args.length > 0) {
+//            port = Integer.parseInt(args[0]);
+//        } else {
+//            port = 9000;
+//        }
+//        new AutobahnServer(port).run();
+//    }
 }
